6fe66e807797ff6a9e173ddba0a6efb8cc223f9c
Catalin <catalin.ples@gmail.com>
2018-05-18 15:29:37 +0300

    changed slidebar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const slideBtton = document.getElementsByClassName("slide-button")[0];
 const number = document.getElementsByClassName("start")[0];
 const blueSlide = document.getElementsByClassName("changed-color")[0];
 const getValues = document.getElementsByClassName("getValues")[0];
+const moveButtonRight = document.querySelector(".move-slider-right");
+const moveButtonLeft = document.querySelector(".move-slider-left");
 
 var initialField = document.getElementsByClassName("initial")[0];
 var initial = 0;
@@ -11,28 +13,36 @@ var last = 100;
 
 slideBar.style.width = "1020px";
 slideBtton.style.left = "0px";
+slideBar.style.left = "0px";
 
 let mouseMove  = false;
 
-document.addEventListener("mousedown", onMouseDown);
+
+slideBar.addEventListener("click", barSlideClick);
+slideBar.addEventListener("mousedown", onMouseDown);
 document.addEventListener("mouseup", onMouseUp);
 document.addEventListener("mousemove", onMouseMove);
 getValues.addEventListener("click", getValuesFunc);
+moveButtonRight.addEventListener("click", moveSlideRight);
+moveButtonLeft.addEventListener("click", moveSlideLeft);
 
 
 function onMouseMove(event) {
-    const oldLeft = slideBtton.style.left.split("px")[0];
+    let initialLeft = parseInt(slideBtton.style.left.split("px")[0]);
+    let actualSlidebarLeft = parseInt(slideBar.style.left.split("px")[0]);
+    let actualSlideLeft = initialLeft; 
     
     if (mouseMove == true) {
-        let xCoords = event.clientX;
+        let xCoords = event.clientX - actualSlidebarLeft;
         if (xCoords < 0) {
-            slideBtton.style.left = "0px";;
+            slideBtton.style.left = "0px";
         } else if (xCoords > 1000) {
             slideBtton.style.left = "1000px";
         } else {
             slideBtton.style.left = xCoords + "px";
-            blueSlide.style.width = (parseInt(oldLeft) + 15) + "px";
+            blueSlide.style.width = (parseInt(actualSlideLeft) +10) + "px";
             number.innerHTML = Math.floor(displayNumber());
+            console.log(xCoords);
         }
     }
 }
@@ -61,4 +71,39 @@ function onMouseUp() {
 function getValuesFunc() {
     initial = parseInt(initialField.value);
     last = parseInt(lastField.value);
+}
+
+function moveSlideRight() {
+    let actualSlidebarLeft = parseInt(slideBar.style.left.split("px")[0]);
+    let addRight = (actualSlidebarLeft + 20) + "px";
+    slideBar.style.left = addRight;
+    console.log(slideBar.style.left);
+    console.log(addRight)
+}
+
+function moveSlideLeft() {
+    let actualSlidebarLeft = parseInt(slideBar.style.left.split("px")[0]);
+    let addRight = (actualSlidebarLeft - 20) + "px";
+    slideBar.style.left = addRight;
+    console.log(slideBar.style.left);
+    console.log(addRight)
+}
+
+function barSlideClick(event) {
+    let initialLeft = parseInt(slideBtton.style.left.split("px")[0]);
+    let actualSlidebarLeft = parseInt(slideBar.style.left.split("px")[0]);
+    let actualSlideLeft = initialLeft; 
+    
+
+        let xCoords = event.clientX - actualSlidebarLeft;
+        if (xCoords < 0) {
+            slideBtton.style.left = "0px";
+        } else if (xCoords > 1000) {
+            slideBtton.style.left = "1000px";
+        } else {
+            slideBtton.style.left = xCoords + "px";
+            number.innerHTML = Math.floor(displayNumber());
+        }
+    actualSlidebarLeft = parseInt(slideBar.style.left.split("px")[0]);
+    blueSlide.style.width = parseInt(slideBtton.style.left.split("px")[0]) + "px";
 }
